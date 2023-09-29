0c5b30185e5f90dcadfec26f11f146f562ddf1b3
Tom Lewis <tom.lewis488@gmail.com>
2023-09-29 07:59:20 +0200

    Add erase button

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ const gridSlider = document.querySelector('.grid-slider');
 const sliderWidth = document.querySelector('.slider-width');
 const sliderHeight = document.querySelector('.slider-height');
 const clearBtn = document.querySelector('.clear-btn');
+const eraserBtn = document.querySelector('.eraser-btn');
 
 let currentGridWidth = 16;
 let currentGridHeight = 16;
+
+let isDrawing = false;
+let isErasing = false;
 
 function makeRows(rows, cols) {
   container.style.setProperty('--grid-rows', rows);
@@ -16,15 +20,38 @@ function makeRows(rows, cols) {
     container.appendChild(cell).className = 'grid-item';
 
     cell.addEventListener('mouseenter', () => {
-      cell.style.background =
-        '#' + parseInt(Math.random() * 0xffffff).toString(16);
+      if (!isDrawing) return;
+      cell.style.background = '';
+      if (!isErasing) {
+      } else {
+        cell.style.background =
+          '#' + parseInt(Math.random() * 0xffffff).toString(16);
+      }
     });
   }
 }
 
+container.addEventListener('mousedown', () => {
+  isDrawing = 'true';
+  isErasing = !isErasing;
+});
+
+container.addEventListener('mouseup', () => {
+  isDrawing = false;
+});
+
+document.addEventListener('mouseup', () => {
+  isDrawing = false; // Set isDrawing to false when the mouse button is released
+});
+
 clearBtn.addEventListener('click', () => {
   container.innerHTML = '';
   makeRows(currentGridWidth, currentGridHeight);
+});
+
+eraserBtn.addEventListener('click', () => {
+  isErasing = !isErasing;
+  eraserBtn.textContent = isErasing ? 'Draw' : 'Erase';
 });
 
 gridSlider.addEventListener('input', () => {
